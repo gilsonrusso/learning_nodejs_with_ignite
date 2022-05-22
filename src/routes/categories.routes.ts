e23b762e826f741da04c780daecc81a9b2862dd7
Gilson Russo <gilsonrusso@outlook.com>
2022-05-22 17:47:49 -0400

    Add multer dependency

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
+import path from "path";
 
 import { createCategoryController } from "../modules/cars/useCases/Categories/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/Categories/listCategories";
 
 export const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: path.resolve(__dirname, "../", "/tmp"),
+});
 
 categoriesRoutes.post("/", async (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,3 +18,14 @@ categoriesRoutes.post("/", async (request, response) => {
 categoriesRoutes.get("/", async (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  "/import",
+  upload.single("file"),
+  async (request, response) => {
+    const { file } = request;
+    console.log(file);
+
+    return response.send();
+  }
+);
