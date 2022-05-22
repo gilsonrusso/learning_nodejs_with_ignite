413c8858349d5d0fc51bf0e99348ccb72f515bad
Gilson Russo <gilsonrusso@outlook.com>
2022-05-21 23:08:20 -0400

    Refactor categoryRoutes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,19 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+// import { PostgresCategoriesRepository } from "../repositories/PostgresCategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepositoy = new CategoriesRepository();
+// const categoriesRepositoy = new PostgresCategoriesRepository();
 
 categoriesRoutes.post("/", async (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = await categoriesRepositoy.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepositoy);
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category already exists" });
-  }
-
-  await categoriesRepositoy.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response
     .status(201)
