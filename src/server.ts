1fd38630a0ad7958ffc1e2f94c2247ee370156e2
Gilson Russo <gilsonrusso@outlook.com>
2022-05-28 15:16:03 -0400

    Add initial configs typeorm and postgres

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
+import "../data-source";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
 import swaggerFile from "./swagger.json";
